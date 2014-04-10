0f948dd209cb7eef22c7b9471e0f26e88190a90d
Seth Siegler <usaseth@gmail.com>
2014-04-09 17:31:39 -0700

    reinstated defaults

--- lib/tourets/agent.rb
+++ lib/tourets/agent.rb
@@ -3,7 +3,7 @@ module TouRETS
     include Utilities
     extend Utilities
     
-    # SEARCH_QUERY_DEFAULTS = {:listing_status => "ER,EA,C", :idx_display => "Y", :internet_display => "Y"}
+    SEARCH_QUERY_DEFAULTS = {}
     # This class searches for ResidentialProperty, Condo, SingleFamily, Rental
     # Some MLS use "1", some use :RES... Will need to decide which way is to be used.
     SEARCH_CONFIG_DEFAULTS = {:search_type => :Agent, :class => "1"}
